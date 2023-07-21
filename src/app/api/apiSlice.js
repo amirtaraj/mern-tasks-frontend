6350de905d03307be5be15bdb77f73ac56d8aa0c
Amirtaraj Ragavan <amirtaraj.r@gmail.com>
2023-07-22 00:00:24 +0530

    mern tasks frontend v1.1

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -9,7 +9,7 @@ const baseQuery = fetchBaseQuery({
         const token = getState().auth.token
 
         if (token) {
-            headers.set("", `Bearer ${token}`)
+            headers.set("authorization", `Bearer ${token}`)
         }
         return headers
     }
